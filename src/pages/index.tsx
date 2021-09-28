a15f16fb6d150b70211cf8fcfcacea1d03b2a1ae
hitaloalvess <hitalo.ralves@outlook.com>
2021-09-28 09:20:14 -0300

    Criando estrutura do projeto next

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ínicio || Dashgo</title>
+      </Head>
+      <h1>Dashgo</h1>
+    </>
+  )
+}
