b71b38a3e0193f858c0982f1d60572da2cd96724
hitaloalvess <hitalo.ralves@outlook.com>
2021-09-28 16:05:20 -0300

    Criando pagina de index e componente header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
 import Head from 'next/head'
+import Input from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -6,7 +8,34 @@ export default function Home() {
       <Head>
         <title>Ínicio | Dashgo</title>
       </Head>
-      <h1>Dashgo</h1>
+      <Flex 
+          as="main" 
+          w="100vw" 
+          h="100vh" 
+          align="center" 
+          justify="center"
+      >
+        <Flex
+          as="form"
+          maxW={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          >
+            <Stack spacing="4">
+             <Input name="email" label="E-mail" type="email" />
+             <Input name="password" label="Senha" type="password" /> 
+              <Button
+                type="submit"
+                colorScheme="pink"
+                size="lg"
+              >
+                Entrar
+              </Button>
+
+            </Stack>
+        </Flex>
+      </Flex>
     </>
   )
 }
