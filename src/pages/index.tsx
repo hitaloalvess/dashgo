77f6a7c4ffc3727dd3462cf4ef0ed3f0f1de032c
hitaloalvess <hitalo.ralves@outlook.com>
2021-09-28 10:41:08 -0300

    Instalando e configurando ChakraUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Ínicio || Dashgo</title>
+        <title>Ínicio | Dashgo</title>
       </Head>
       <h1>Dashgo</h1>
     </>
