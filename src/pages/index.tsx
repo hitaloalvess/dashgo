67c71ccafdb4bb1c20e8430f60b7c1fb9506950f
hitaloalvess <hitalo.ralves@outlook.com>
2021-09-30 21:00:28 -0300

    Adicionando funcionalidade ao form do login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
-import { Flex, Button, Stack } from '@chakra-ui/react'
-import Head from 'next/head'
-import Input from '../components/Form/Input'
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import Head from 'next/head';
+import { Input }  from '../components/Form/Input';
+
+type SignInFormData = {
+  email:string;
+  password: string;
+}
 
 export default function Home() {
+
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn : SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise( resolve => setTimeout(resolve, 2000) )
+    console.log(values)
+  }
+
   return (
     <>
       <Head>
@@ -21,14 +36,16 @@ export default function Home() {
           bg="gray.800"
           p="8"
           borderRadius={8}
+          onSubmit={handleSubmit(handleSignIn)}
           >
             <Stack spacing="4">
-             <Input name="email" label="E-mail" type="email" />
-             <Input name="password" label="Senha" type="password" /> 
+             <Input name="email" label="E-mail" type="email" {...register('email')} />
+             <Input name="password" label="Senha" type="password" {...register('password')} /> 
               <Button
                 type="submit"
                 colorScheme="pink"
                 size="lg"
+                isLoading={formState.isSubmitting}
               >
                 Entrar
               </Button>
